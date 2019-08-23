a18be6d3ec90406b46e93ac1691bfc19994fe833
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-23 10:48:57 -0500

    added characters list no pics yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
 
 
 export default function App() {
-  return (
+  return ([
     <main>
       <Header />
       {/* <TabNav /> */}
-    </main>
-  );
+      <CharacterList />
+    </main>,
+    
+  ]);
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Card, Grid } from 'semantic-ui-css';
-export default function CharacterCard() {
+import { Card, Grid } from 'semantic-ui-react';
+export default function CharacterCard(props) {
   return (
-    
+  <Grid.Column padded key={props.id}>
+    <Card>
+      <Card.Content>
+        <Card.Header>Name: {props.name}</Card.Header>
+        <Card.Description>Status: {props.status}</Card.Description>
+        <Card.Description>Species: {props.species}</Card.Description>
+      </Card.Content>
+    </Card>
+  </Grid.Column>
   )
 }
